bfd7e404c1b80ab14065d4b7c425abf4e99f7046
Mikhail Ivanov <moveout@yandex.ru>
2022-06-26 18:53:41 +0300

    Задание 2

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_ws(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.l_grades:
+                lecturer.l_grades[course] += [grade]
+            else:
+                lecturer.l_grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.l_grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -24,22 +44,19 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    pass
-
-
-class Reviever(Mentor):
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
-
 cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer = Reviewer('Any', 'Body')
+cool_lecturer = Lecturer('No', 'One')
+cool_lecturer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Python']
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+best_student.rate_ws(cool_lecturer, 'Python', 9)
+best_student.rate_ws(cool_lecturer, 'Python', 10)
+best_student.rate_ws(cool_lecturer, 'Python', 8)
 
 print(best_student.grades)
+print(cool_lecturer.l_grades)
